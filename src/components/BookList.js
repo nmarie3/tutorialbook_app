eb9c647a0d4f358a3ac8b621a89d58384099fa70
coga0057 <natasha.marie33@gmail.com>
2024-03-22 17:29:58 +0900

    still has errors

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,17 @@
-import BookShow from './BookShow'
+import {useContext} from 'react';
+import BooksContext from '../context/books';
+import BookShow from './BookShow';
 
 
-function BookList({books, onDelete, onEdit}) {
+function BookList() {
+    const {books} = useContext(BooksContext);
+
     const renderedBooks = books.map((book) => {
-        return <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book}/>;
+        return <BookShow key={book.id} book={book}/>;
     });
     return (
-        <div className="book-list">{renderedBooks}</div>
+        <div className="book-list">
+            {renderedBooks}</div>
     )
 }
 
